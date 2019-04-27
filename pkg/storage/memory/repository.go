80d3936522b3bd4506008d73d1b548cf72516154
Otto Schuldt <ottoschuldt@gmail.com>
2019-04-28 01:10:26 +0200

    improves text in error

--- pkg/storage/memory/repository.go
+++ pkg/storage/memory/repository.go
@@ -75,7 +75,7 @@ func (m *Storage) UpdateRiskMatrixSize(riskMatrixID, newImageWidth int ) error {
 			return nil
 		}
 	}
-	return errors.New("risk matrix not found")
+	return errors.New(fmt.Sprintf("risk matrix not found with ID: %v", riskMatrixID))
 }
 
 // AddRisk saves the given risk in the repository
